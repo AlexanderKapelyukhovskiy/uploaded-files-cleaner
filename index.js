e8847236247367d61305a1304fd9512e94411ff3
Alex Kapelyukhovsky <alkapa2005@gmail.com>
2018-03-22 18:40:52 +0200

    Add eslint and format file accordingly

--- index.js
+++ index.js
@@ -1,69 +1,70 @@
-var watch = require('node-watch');
-var fs = require('fs');
-var path = require('path');
+const watch = require('node-watch')
+const fs = require('fs')
+const path = require('path')
 
-const folderToWatch = process.env['folderToWatch'] || process.argv[2];
-const deleteNewFileAfterSec = process.env['deleteNewFileAfterSec'] || process.argv[3];
-const logPath = process.env['logPath'] || process.argv[4] || './log.txt';
-const clearFolderOnInit = process.env['clearFolderOnInit'] || process.argv[5] || false;
+const folderToWatch = process.env['folderToWatch'] || process.argv[2]
+const deleteNewFileAfterSec = process.env['deleteNewFileAfterSec'] || process.argv[3]
+const logPath = process.env['logPath'] || process.argv[4] || './log.txt'
+const clearFolderOnInit = process.env['clearFolderOnInit'] || process.argv[5] || false
 
 const log = msg => {
-    console.log(msg);
-    fs.appendFileSync(logPath, `${new Date()} - ${msg}\n`);
-};
+  console.log(msg)
+  fs.appendFileSync(logPath, `${new Date()} - ${msg}\n`)
+}
+
 
 const clearFolder = directory => {
-    log('clearFolder...');
+  log('clearFolder...')
 
-    fs.readdir(directory, (err, files) => {
-        log('readdir...');
-        
-        if (err) {
-            console.log('Error3: ' + err);
-            throw err;
-        }
+  fs.readdir(directory, (err, files) => {
+    log('readdir...')
 
-        for (const file of files) {
-            const f = path.join(directory, file);
-            log(`${f} is removing`);
-            fs.unlink(f, err => {
-                log(`${f} was removed`);
-                if (err) {
-                    console.log('Error4: ' + err);
-                    throw err;
-                }
-            });
-        }
-    });
-};
-
-log(`Starting ${folderToWatch} ${deleteNewFileAfterSec} ${clearFolderOnInit}...`);
-
-try {
-    if (clearFolderOnInit == 'true') {
-        log('Clearing Folder OnInit...');
-        clearFolder(folderToWatch);
+    if (err) {
+      console.log('Error3: ' + err)
+      throw err
     }
 
-    watch(folderToWatch, { recursive: true }, function(evt, name) {
-        try {
-            log(`${name} changed.`);
-            setTimeout(() => {
-                if (fs.existsSync(name)) {
-                    fs.unlinkSync(name);
-                    log(`${name} deleted.`);
-                }
-            }, parseInt(deleteNewFileAfterSec) * 1000);
-        } catch (e) {
-            log('Error2');
-            log(e);
-            log(e.stack);
-            process.exit(-2);
-        }    
-    });
-} catch(e) {
-    log('Error1');
-    log(e);
-    log(e.stack);
-    process.exit(-1);
+    for (const file of files) {
+      const f = path.join(directory, file)
+      log(`${f} is removing`)
+      fs.unlink(f, err => {
+        log(`${f} was removed`)
+        if (err) {
+          console.log('Error4: ' + err)
+          throw err
+        }
+      })
+    }
+  })
+}
+
+log(`Starting ${folderToWatch} ${deleteNewFileAfterSec} ${clearFolderOnInit}...`)
+
+try {
+  if (clearFolderOnInit === 'true') {
+    log('Clearing Folder OnInit...')
+    clearFolder(folderToWatch)
+  }
+
+  watch(folderToWatch, {recursive: true}, (evt, name) => {
+    try {
+      log(`${name} changed.`)
+      setTimeout(() => {
+        if (fs.existsSync(name)) {
+          fs.unlinkSync(name)
+          log(`${name} deleted.`)
+        }
+      }, parseInt(deleteNewFileAfterSec, 10) * 1000)
+    } catch (e) {
+      log('Error2')
+      log(e)
+      log(e.stack)
+      process.exit(-2)
+    }
+  })
+} catch (e) {
+  log('Error1')
+  log(e)
+  log(e.stack)
+  process.exit(-1)
 }
--- install.js
+++ install.js
@@ -1,28 +1,28 @@
-var Service = require('node-windows').Service;
-var EventLogger = require('node-windows').EventLogger;
+const Service = require('node-windows').Service
+const EventLogger = require('node-windows').EventLogger
 
-const serviceName = 'Uploaded Files Cleaner';
+const serviceName = 'Uploaded Files Cleaner'
 
-var logger = new EventLogger(serviceName);
+const logger = new EventLogger(serviceName)
 
-const runInstall = process.argv.length == 7;
-const folderToWatch =  runInstall ? process.argv[3] : '';
-const deleteNewFileAfterSec = runInstall ? process.argv[4] : 0;
-const logPath = process.argv[5] || '';
-const clearFolderOnInit = process.argv[6] || false;
-const action = process.argv[2];
+const runInstall = process.argv.length === 7
+const folderToWatch = runInstall ? process.argv[3] : ''
+const deleteNewFileAfterSec = runInstall ? process.argv[4] : 0
+const logPath = process.argv[5] || ''
+const clearFolderOnInit = process.argv[6] || false
+const action = process.argv[2]
 
 const log = msg => {
-  console.log(msg);
-  logger.info(msg);
-};
+  console.log(msg)
+  logger.info(msg)
+}
 
 if (runInstall) {
   log(
     `Run install with ${action} for ${folderToWatch} and ${deleteNewFileAfterSec} sec interval.`);
 }
 
-var svc = new Service({
+const svc = new Service({
   name: serviceName,
   description: 'Remove files from selected folder after selected time.',
   script: './index.js',
@@ -39,30 +39,30 @@ var svc = new Service({
     name: 'clearFolderOnInit',
     value: clearFolderOnInit
   }]
-});
+})
 
-svc.on('install', function(){
-  log('Service Installed.');
-  svc.start();
-  log('Service is starting...');
-});
+svc.on('install', () => {
+  log('Service Installed.')
+  svc.start()
+  log('Service is starting...')
+})
 
-svc.on('uninstall', function(){
-  log('Service Uninstalled.');
-});
+svc.on('uninstall', () => {
+  log('Service Uninstalled.')
+})
 
 svc.on('start', () => {
-  log('Service started...');
-});
+  log('Service started...')
+})
 
 svc.on('stop', () => {
-  log('Service stopped...');
-});
+  log('Service stopped...')
+})
 
-if (action == 'install') {
-    log('Run Install...');
-    svc.install();
+if (action === 'install') {
+  log('Run Install...')
+  svc.install()
 } else {
-    log('Run Uninstall...');
-    svc.uninstall();
+  log('Run Uninstall...')
+  svc.uninstall()
 }
